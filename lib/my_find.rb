db5ffbc768a2749edc8b06eaaf3727eb97b1598e
Matthew Corralejo <mattcorralejo1@gmail.com>
2019-12-17 01:02:04 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,16 +1,9 @@
-
-
 def my_find(collection)
-i = 0
-while i <
-
-collection.length
-
-return collection[i]
-if yield(collection[i])
-
-i = i +1
-end
+  i = 0
+  while i < collection.length
+    return collection[i] if yield(collection[i])
+    i = i + 1
+  end
 end
 collection = (1..100).to_a
 my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 }
